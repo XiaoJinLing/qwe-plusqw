c5ce6c104b874363ad6b3b7e401fc6d42c892f69
栗子 <1224145956@qq.com>
2021-10-09 14:42:26 +0800

    123

--- src/main/java/com/xiao/bl/controller/qwe.java
+++ src/main/java/com/xiao/bl/controller/qwe.java
@@ -1,5 +1,11 @@
 package com.xiao.bl.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class qwe {
+    @RequestMapping("/qwe")
+    public void re(){
+
+    }
 
 }
